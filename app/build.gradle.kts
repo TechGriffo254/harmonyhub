7ecac1116527bc07d3823035c336b21eb02056b7
Techgriffo254 <griffotech254@gmail.com>
2024-09-25 09:42:24 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,112 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+//    alias(libs.plugins.)
+}
+
+android {
+    namespace = "com.techgriffo254.harmonyhub"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.techgriffo254.harmonyhub"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    implementation(libs.gson)
+    implementation(libs.androidx.browser)
+    implementation(libs.androidx.datastore.core.android)
+
+    testImplementation(libs.junit)
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //extended icons
+    implementation(libs.androidx.material.icons.extended.android)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Coroutine Lifecycle Scopes
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //Okhttp
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+
+    // Add Google Sign-In dependency
+    implementation(libs.play.services.auth)
+
+    // Add Coil for image loading
+    implementation(libs.coil.compose)
+
+    // Add Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.room.compiler)
+
+    // Add KSP
+    ksp(libs.androidx.room.room.compiler)
+
+    // compose navigation
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.datastore.preferences.core)
+    implementation(libs.androidx.datastore.preferences)
+
+    implementation (libs.exoplayer.core)
+    implementation (libs.exoplayer.ui)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
+    implementation(libs.androidx.media3.ui)
+}
---
+++ app/src/main/java/com/techgriffo254/harmonyhub/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.techgriffo254.harmonyhub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val dark = Color(0xFF0F0817)
+val Pink = Color(0xFFDB28A9)
+val Purple1 = Color(0xFF9D1DCA)
+val Purple2 = Color(0xFF892FE0)
+val Grey = Color(0xFF433E48)
+val Grey2 = Color(0xFFA5A5A5)
